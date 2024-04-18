de754c8af9313ac38c7f10fe1556cf2bebb7436c
Joydip <joydip.mondal@gmail.com>
2024-04-18 10:15:04 +0530

    Update product.service.ts

--- codes/day-4/pms-app-v2/src/app/products/services/product.service.ts
+++ codes/day-4/pms-app-v2/src/app/products/services/product.service.ts
@@ -8,60 +8,29 @@ import { ApiResponse } from "../../models/apiresponse";
 
 @Injectable()
 export class ProductService implements DataService {
-    //private url: string = ''
 
     constructor(
         @Inject(PRODUCT_URL_TOKEN) private _url: string,
         private _http: HttpClient
     ) {
-        //this.url = _url
         console.log('service created')
     }
 
-    getProducts(): Observable<Product[]> {
-        const obs: Observable<Product[]> = this._http.get<Product[]>(this._url)
-        return obs;
+    getProducts(): Observable<ApiResponse<Product[]>> {
+        return this._http.get<ApiResponse<Product[]>>(this._url)
     }
-    getProductById(id: string): Observable<Product> {
-        return this._http.get<Product>(`${this._url}/${id}`)
-        // const obs: Observable<any> = this._http.get(`${this._url}/${id}`)
-        // const resObs: Observable<Product> = obs.pipe(
-        //     map(
-        //         (responseData: any): Product => {
-        //             return {
-        //                 id: responseData.id,
-        //                 productCode: responseData.productCode,
-        //                 productName: responseData.productName,
-        //                 description: responseData.description,
-        //                 releaseDate: responseData.releaseDate,
-        //                 price: responseData.price,
-        //                 starRating: responseData.starRating,
-        //                 imageUrl: responseData.imageUrl
-        //             }
-        //         }
-        //     )
-        // )
-        //return resObs
-    }
-    addProduct(product: Product): Observable<ApiResponse<Product>> {
-        const obs = this._http.post(this._url, product)
-        const resObs = obs.pipe(
-            map(
-                (responseData): ApiResponse<Product> => {
-                    return {
-                        statusCode: 201,
-                        statusText: 'created',
-                        data: <Product>responseData
-                    }
-                }
-            )
-        )
-        return resObs
-    }
-    updateProduct(product: Product, id: string) {
 
+    getProductById(id: number): Observable<ApiResponse<Product>> {
+        return this._http.get<ApiResponse<Product>>(`${this._url}/${id}`)
     }
-    deleteProduct(id: string) {
 
+    addProduct(product: Product): Observable<ApiResponse<Product[]>> {
+        return this._http.post<ApiResponse<Product[]>>(this._url, product)
+    }
+    updateProduct(product: Product, id: number): Observable<ApiResponse<Product[]>> {
+        return this._http.put<ApiResponse<Product[]>>(`${this._url}/${id}`, product)
+    }
+    deleteProduct(id: number): Observable<ApiResponse<Product[]>> {
+        return this._http.delete<ApiResponse<Product[]>>(`${this._url}/${id}`)
     }
 }
